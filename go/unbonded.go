f8cb6649135ebe02ee1d88849a3121abd1702f9d
hxyv <xingyu3906@gmail.com>
2024-11-29 15:52:48 -0500

    Update unbond energy force

--- go/unbonded.go
+++ go/unbonded.go
@@ -22,15 +22,6 @@ func CalculateLJPotentialEnergy(B, A, r float64) float64 {
 	return (A / r_6) - (B / r_12)
 }
 
-// func CalculateLJForce take the result of CalculateLJPotentialEnergy
-// return the LJForce between two atoms
-func CalculateLJForce(A, B, r float64) float64 {
-	r_6 := math.Pow(r, 6)
-	r_12 := r_6 * r_6
-
-	return (A / r_12) - (B / r_6)
-}
-
 // C: coefficient 1
 // D: coefficient 2
 // r: distance between atom 1 and atom 2
@@ -134,4 +125,89 @@ func CalculateTotalUnbondEnergy(p *Protein, nonbondedParameter parameterDatabase
 	}
 
 	return energyMap
+}
+
+func CalculateTotalUnbondedEnergyForce(p *Protein, nonbondedParameter parameterDatabase) (float64, map[int]*TriTuple) {
+	forceMap := make(map[int]*TriTuple)
+	totalEnergy := 0.0
+	verletList := NewVerletList()
+	verletList.BuildVerlet(p)
+
+	for _, residue := range p.Residue {
+		for _, atom1 := range residue.Atoms {
+			// Initialize force for atom1
+			forceMap[atom1.index] = &TriTuple{0.0, 0.0, 0.0}
+
+			// Access the Neighbors map using the dereferenced verletList
+			neighbors, exists := verletList.Neighbors[atom1]
+			if !exists {
+				continue
+			}
+
+			for _, atom2 := range neighbors {
+				// Compute the distance between atom1 and atom2
+				r := Distance(atom1.position, atom2.position)
+
+				// Calculate the electric potential energy between atom1 and atom2
+				electricPotentialEnergy := CalculateElectricPotentialEnergy(atom1, atom2, r)
+				totalEnergy += electricPotentialEnergy
+
+				// Calculate the electric force between atom1 and atom2
+				electricForce := CalculateElectricForce(atom1, atom2, r)
+
+				// Update the force map for atom1
+				forceMap[atom1.index].x += electricForce.x
+				forceMap[atom1.index].y += electricForce.y
+				forceMap[atom1.index].z += electricForce.z
+
+				// Calculate the Lennard-Jones potential energy between atom1 and atom2
+				parameterList := SearchParameter(2, nonbondedParameter, atom1, atom2)
+				if len(parameterList) == 2 {
+					LJPotentialEnergy := CalculateLJPotentialEnergy(parameterList[0], parameterList[1], r)
+					totalEnergy += LJPotentialEnergy
+
+					// Calculate the Lennard-Jones force between atom1 and atom2
+					LJForce := CalculateLJForce(atom1, atom2, parameterList[0], parameterList[1], r)
+
+					// Update the force map for atom1
+					forceMap[atom1.index].x += LJForce.x
+					forceMap[atom1.index].y += LJForce.y
+					forceMap[atom1.index].z += LJForce.z
+				}
+			}
+		}
+	}
+
+	return totalEnergy, forceMap
+}
+
+func CalculateElectricForce(a1, a2 *Atom, r float64) TriTuple {
+	forceMagnitude := (a1.charge * a2.charge) / (4 * math.Pi * epsilon * r * r * 1e-10)
+	unitVector := TriTuple{
+		x: (a2.position.x - a1.position.x) / r,
+		y: (a2.position.y - a1.position.y) / r,
+		z: (a2.position.z - a1.position.z) / r,
+	}
+	return TriTuple{
+		x: forceMagnitude * unitVector.x,
+		y: forceMagnitude * unitVector.y,
+		z: forceMagnitude * unitVector.z,
+	}
+}
+
+func CalculateLJForce(a1, a2 *Atom, A, B, r float64) TriTuple {
+	r_6 := math.Pow(r, 6)
+	r_12 := r_6 * r_6
+
+	forceMagnitude := (12*B/r_12 - 6*A/r_6)
+	unitVector := TriTuple{
+		x: (a2.position.x - a1.position.x) / r,
+		y: (a2.position.y - a1.position.y) / r,
+		z: (a2.position.z - a1.position.z) / r,
+	}
+	return TriTuple{
+		x: forceMagnitude * unitVector.x,
+		y: forceMagnitude * unitVector.y,
+		z: forceMagnitude * unitVector.z,
+	}
 }
